5088c6c0e5d59a60f3c8bd81a0b74556d1e502eb
Shannan Bunch <shannan.bunch@tvgla.com>
2020-08-31 15:02:33 -0700

    Updates

---
+++ precache-manifest.35987f3056f3822808a3a982fb47358e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a1c85fff1716c03e0fb88d5ef5fd7b",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "c5fa7e63680c4acf48a1",
+    "url": "/react-movie-search/static/css/main.b2846306.chunk.css"
+  },
+  {
+    "revision": "452d61dff2e818070266",
+    "url": "/react-movie-search/static/js/2.28316594.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.28316594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fa7e63680c4acf48a1",
+    "url": "/react-movie-search/static/js/main.e6374538.chunk.js"
+  },
+  {
+    "revision": "0f96aaaceff1bc618c94",
+    "url": "/react-movie-search/static/js/runtime-main.daddf18a.js"
+  }
+]);
